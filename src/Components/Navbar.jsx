d9bd5a510010a0d70b12c08c5609a01a99964f02
VivDev2 <vivekmahajan256@gmail.com>
2023-03-07 16:42:57 +0530

    this is the react app code

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, {useState}from 'react';
 import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai';
-import mhlogo from '../assets/mhlogo.jpeg'
+
 
 const Navbar = () => {
   const [nav,setNav]  = useState(false)
